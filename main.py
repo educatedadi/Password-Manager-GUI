33871d006362e045e20e749f5b90b70ce43e82bf
educatedadi <93342026+educatedadi@users.noreply.github.com>
2021-12-08 23:06:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ website_entry.grid(row=1, column=1, pady=5, sticky="EW")
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, pady=5, sticky="EW")
-email_entry.insert(0, 'aditya.raj111@yahoo.com')
+email_entry.insert(0, 'your_mail@gmail.com')
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, pady=5, sticky="EW")
